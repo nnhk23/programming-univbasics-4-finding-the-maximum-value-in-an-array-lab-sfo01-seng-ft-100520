cff840c8a0d6179fd72f05ecc65a02fb11de8a06
Kim Nguyen <nnhk23@gmail.com>
2020-09-10 20:53:51 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,11 @@
 def find_max_value(array)
-  
+  i = 0 
+  max = []
+  while i < array.length do
+    if array[i] > array[i+1]
+      max = array[i]
+      elsif array[i+1] > array[i]
+      max = array[i+1]
+  end 
+  return max
 end
