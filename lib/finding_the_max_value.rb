212bccc17ccda34889f4830168394be85f3a11cd
Kim Nguyen <nnhk23@gmail.com>
2020-09-10 21:08:17 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -4,9 +4,10 @@ def find_max_value(array)
   while i < array.length do
     max = array[i-1]
     if array[i] > max
-      max = array[i]
+      return max = array[i]
+      elsif max > array[i]
+      return max
   end 
   i += 1 
 end 
-puts max
 end
